d7eaadcb796871ccc3ada6b9279389d772e1c480
kadirdolek <kadirdolek@outlook.com>
2025-05-20 13:04:00 +0200

    step2

--- src/Components/Tableau/Tableau.jsx
+++ src/Components/Tableau/Tableau.jsx
@@ -6,7 +6,36 @@ export default function Tableau(){
     return(
 
         <div id='tableau'>
-            
+            <div id='container'>
+                <div id='stepp1'>
+                    <p id='pnumber'>1</p>
+                    <div id='step1bis'>
+                        <p id='step1p1'>STEP 1</p>
+                        <p id='step1p2'>YOUR INFO</p>
+                    </div>
+                </div>
+                <div id='stepp2'>
+                    <p id='pnumber'>2</p>
+                    <div id='step1bis'>
+                        <p id='step1p1'>STEP 2</p>
+                        <p id='step1p2'>SELECT A PLAN</p>
+                    </div>
+                </div>
+                <div id='stepp3'>
+                    <p id='pnumber'>3</p>
+                    <div id='step1bis'>
+                        <p id='step1p1'>STEP 3</p>
+                        <p id='step1p2'>ADD-ONS</p>
+                    </div>
+                </div>
+                <div id='stepp4'>
+                    <p id='pnumber'>4</p>
+                    <div id='step1bis'>
+                        <p id='step1p1'>STEP 4</p>
+                        <p id='step1p2'>SUMMARY</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
