81a59757fdb6a2c89850628d3f173d84e981cf63
kadirdolek <kadirdolek@outlook.com>
2025-05-20 18:05:34 +0200

    step5

--- src/Components/Step5/Step5.jsx
+++ src/Components/Step5/Step5.jsx
@@ -12,9 +12,9 @@ export default function Step5(){
             <div>
                 <img src="./src/assets/Img/thankyou.svg" alt="" />
             </div>
-            <div>
+            <div style={{textAlign:'center', paddingLeft:'12.5%', paddingRight:'12.5%'}}>
                 <h1>Thank you!</h1>
-                <p></p>
+                <p style={{ opacity:'70%'}}>Thank for you confirming your subscription! We hope you had fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com</p>
             </div>
         </div>
     )
